2b0603d488845a3e39e833f02d80f08afc65966b
Unknown <1061129075@qq.com>
2018-11-21 23:43:30 +0800

    merge sort complete but it's wrong

--- Sort/Sort/Sort/Sort.cpp
+++ Sort/Sort/Sort/Sort.cpp
@@ -2,15 +2,19 @@
 //
 
 #include "pch.h"
+
+#include <cstdlib>
 #include <iostream>
 
 void QuickSort(int*, int left, int right);	//这里采用比较基本的方法，但是入口要一直传递大数组
+void MergeSort(int*, int left, int right);
 void Swap(int &, int &);
 
 int main()
 {
 	int arr[10] = { 3,1,6,4,5,9,5,10,7,2};
-	QuickSort(arr, 0, 9);
+	//QuickSort(arr, 0, 9);
+	MergeSort(arr, 0, 9);
 	for (int i = 0; i < 10; i++) {
 		std::cout << arr[i] << std::endl;
 	}
@@ -39,6 +43,33 @@ void QuickSort(int* arr, int left, int right) {
 	QuickSort(arr, 0, left - 1);
 	QuickSort(arr, left + 1, max);
 }
+
+void MergeSort(int* arr, int left, int right) {
+	if (right == left)
+		return;
+	int mid = (right + left - 1) / 2;
+	MergeSort(arr, left, mid);
+	MergeSort(arr, mid + 1, right);
+
+	int* temp = new int(right - left + 1);
+
+	int i = left, j = mid + 1;
+	int n = 0;
+	while (i <= mid && j <= right) {
+		if (arr[i] <= arr[j])
+			temp[n++] = arr[i++];
+		else
+			temp[n++] = arr[j++];
+	}
+	while (i<=mid)
+		temp[n++] = arr[i++];
+	while (j <= right)
+		temp[n++] = arr[j++];
+	for (int i = 0; i < n; i++)
+		arr[i + left] = temp[i];
+	delete temp;
+}
+
 
 void Swap(int &a, int &b) {
 	if (a == b)
